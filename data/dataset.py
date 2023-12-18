c384d19492bb5a4890ae9541ab33412c31491790
Yubelo3 <2841144390@qq.com>
2023-12-18 16:04:57 +0800

    123

--- data/dataset.py
+++ data/dataset.py
@@ -5,7 +5,11 @@ from tqdm import tqdm
 import numpy as np
 import warnings
 from torch.utils.data import Dataset, DataLoader
-from loss.tmalign import tm_score
+import sys
+sys.path.append("/home/Yubel/bioinfo/project/")
+from data.tmalign import tm_score
+import torch
+from torch.nn.utils.rnn import pad_sequence
 
 
 def pdbfile2fasta(path):
@@ -30,19 +34,23 @@ def load_database(target_file="data/database_full.npz",db_folder="pdb/"):
         return res["seq"],res["endpoints"],res["filepaths"]
     
     seq,endpoints,filepaths=[],[0],[]  # 待存取信息
-    bar=tqdm(os.listdir(db_folder))
+    # bar=tqdm(os.listdir(db_folder))
+    bar=os.listdir(db_folder)
     process_cnt=0
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore")
         for folder in bar:
             for file in os.listdir(db_folder+folder):
                 filepath=folder+"/"+file
-                embedded_protein=fasta2int(pdbfile2fasta(db_folder+filepath))
-                endpoints.append(endpoints[-1]+len(embedded_protein))
-                seq.extend(embedded_protein)
-                filepaths.append(filepath)
+                try:
+                    embedded_protein=fasta2int(pdbfile2fasta(db_folder+filepath))
+                    endpoints.append(endpoints[-1]+len(embedded_protein))
+                    seq.extend(embedded_protein)
+                    filepaths.append(filepath)
+                except:
+                    print("invalid: "+filepath)
                 process_cnt+=1
-                bar.set_description(f"{process_cnt}/107160 || {process_cnt/107160*100:.4f}%")
+                # bar.set_description(f"{process_cnt}/107160 || {process_cnt/107160*100:.4f}%")
             # if process_cnt>=300:
             #     break
     np.savez_compressed(target_file,seq=seq,endpoints=endpoints,filepaths=filepaths)
@@ -56,31 +64,35 @@ class PDBDataset(Dataset):
         self.filepaths=filepaths
     
     def __getitem__(self, index):
-        return self.seq[endpoints[index]:endpoints[index+1]],self.filepaths[index]
+        return (torch.LongTensor(self.seq[endpoints[index]:endpoints[index+1]]),
+                self.filepaths[index])
 
     def __len__(self):
         return len(self.filepaths)
 
 def collate_fn(batch):
-    tm_score=
-    print(batch)
-    # print(batch[1])
-    exit()
+    n=len(batch)
+    tm_scores=torch.zeros(n,n)
+    for i in range(n):
+        for j in range(n):
+            tm_scores[i,j]=tm_score("pdb/"+batch[i][1],"pdb/"+batch[j][1])
+    seq=pad_sequence([x[0] for x in batch],batch_first=True,padding_value=ord('Z')-ord('A')+1)
+    return seq, tm_scores
 
 
 if __name__=="__main__":
-    seq,endpoints,filepaths=load_database("data/database.npz")
+
+    seq,endpoints,filepaths=load_database("data/database_full.npz")
     # print(seq[:10])
     # print(endpoints[0])
     # print(endpoints[1])
     # print(endpoints[2])
     # print(filepaths[0])
-    dataset=PDBDataset(seq,endpoints,filepaths)
-    loader=DataLoader(dataset,batch_size=16,shuffle=True,collate_fn=collate_fn)
-    for x in loader:
-        print(x[0])
-        print(x[1])
-        break
+    # dataset=PDBDataset(seq,endpoints,filepaths)
+    # loader=DataLoader(dataset,batch_size=16,shuffle=True,collate_fn=collate_fn)
+    # for x in loader:
+    #     print(x[0].shape)
+    #     print(x[1].shape)
 
 
 
